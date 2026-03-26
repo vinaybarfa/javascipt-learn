57ee8ab6ae3296821ff0dc024507d0e08299f10e
Vinay Barfa <vinaybarfa54@gmail.com>
2026-03-26 09:17:42 +0000

    date in js

---
+++ 01_basics/06_data.js
@@ -0,0 +1,15 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.getMonth()+1)
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+
+
+let myCreatedDate = new Date(2025, 4, 12, 8, 23,56,545)
+console.log(myCreatedDate);
+console.log(myCreatedDate.getFullYear());
+console.log(myCreatedDate.getDay());
+
+
+
